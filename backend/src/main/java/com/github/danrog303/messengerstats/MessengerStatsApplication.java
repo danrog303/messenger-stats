6b6fed33c115f53cf5a43ef7ddb351e3d262f64d
Daniel Rogowski <daniel.rogowski@onet.pl>
2024-03-26 15:35:31 +0100

    Initial commit

---
+++ backend/src/main/java/com/github/danrog303/messengerstats/MessengerStatsApplication.java
@@ -0,0 +1,13 @@
+package com.github.danrog303.messengerstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerStatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/danrog303/messengerstats/MessengerstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danrog303.messengerstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
