af1021751215ec20cbe3e596ce47b73f79200f5b
Daniel Rogowski <daniel.rogowski@onet.pl>
2024-04-10 21:43:27 +0200

    Add metric name to stats service output

--- stats-service/chatstat_api.py
+++ stats-service/chatstat_api.py
@@ -12,43 +12,43 @@ workdir = "C:\\Users\\Daniel Rogowski\\Desktop\\elo"
 def get_stats(chatstat: chatstat_lib.ChatStat):
     time_df = chatstat.generate_time_indexed_df(chatstat.msg_df)
 
-    metrics = chatstat.biggest_chat()
+    metrics = dict(type="biggest-chat", metrics=chatstat.biggest_chat())
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.sent_from(omit_first=True)
+    metrics = dict(type="sent-from", metrics=chatstat.sent_from(omit_first=True))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.msg_types()
+    metrics = dict(type="msg-types", metrics=chatstat.msg_types())
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.yearly_graph(time_df)
+    metrics = dict(type="yearly-graph", metrics=chatstat.yearly_graph(time_df))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.hourly_graph(time_df)
+    metrics = dict(type="hourly-graph", metrics=chatstat.hourly_graph(time_df))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.monthly_graph(time_df)
+    metrics = dict(type="monthly-graph", metrics=chatstat.monthly_graph(time_df))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.minutely_graph(time_df)
+    metrics = dict(type="minutely-graph", metrics=chatstat.minutely_graph(time_df))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.daily_graph(time_df)
+    metrics = dict(type="daily-graph", metrics=chatstat.daily_graph(time_df))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.weekday_graph(time_df)
+    metrics = dict(type="weekday-graph", metrics=chatstat.weekday_graph(time_df))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.chat_counts()
+    metrics = dict(type="chat-counts", metrics=chatstat.chat_counts())
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.word_counts(length=3)
+    metrics = dict(type="word-counts-3", metrics=chatstat.word_counts(length=3))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.word_counts(length=5)
+    metrics = dict(type="word-counts-5", metrics=chatstat.word_counts(length=5))
     yield f"data: {json.dumps(metrics)}\n\n"
 
-    metrics = chatstat.word_counts(length=7)
+    metrics = dict(type="word-counts-7", metrics=chatstat.word_counts(length=7))
     yield f"data: {json.dumps(metrics)}\n\n"
 
 
